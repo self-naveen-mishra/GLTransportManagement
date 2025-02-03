1d3cdbfb8945dfdd0a0041d25fe2fb7b2d8a080e
self-naveen-mishra <naveen.c.mishra@globallogic.com>
2025-02-03 12:27:25 +0530

    added vendor django app

--- employee_driver_management_project/urls.py
+++ employee_driver_management_project/urls.py
@@ -22,6 +22,8 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('employee_management.urls')),
+    path('', include('vendor_management.urls')),
+    
     
 ]
 
